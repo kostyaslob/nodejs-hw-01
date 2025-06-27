2c2aee0bc11c4058017d5c7abe739acd50990ecf
kostyaslob <kostyaslob@gmail.com>
2025-06-27 21:05:46 +0300

    some fix-2

--- src/index.js
+++ src/index.js
@@ -1,14 +1,2 @@
-import { readContacts } from "./utils/readContacts.js";
-import { writeContacts } from "./utils/writeContacts.js";
 
-async function main() {
-    const contacts = await readContacts();
-    
-    contacts.push({ id: 3, name: "Kostya" });
-
-    await writeContacts(contacts);
-
-}
-
-main();
 
