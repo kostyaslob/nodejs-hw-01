799c7fc3bfe40215cf6c8d4606fabb543e6b623a
kostyaslob <kostyaslob@gmail.com>
2025-06-27 20:40:56 +0300

    step-6

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -1,3 +1,8 @@
-export const countContacts = async () => {};
+import {readContacts} from "../utils/readContacts.js";
+
+export const countContacts = async () => {
+    const contactsQuantity = await readContacts();
+    return contactsQuantity ? contactsQuantity.length : 0;
+};
 
 console.log(await countContacts());
